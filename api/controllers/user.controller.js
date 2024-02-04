a53cac677789280fb592dcbc717377b7d26f065b
iamdul <kalharidulanga@gmail.com>
2024-02-04 20:43:03 +0530

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,7 +8,9 @@ export const test=(req,res)=>{
 }
 
 export const updateUser= async (req,res,next) =>{
+    
     if(req.user.id !== req.params.userId){
+
         return next(errorHandler(403,'You are not allowed to update this user'));
     }
     if(req.body.password){
@@ -46,4 +48,20 @@ export const updateUser= async (req,res,next) =>{
         next(error);
     }
 
+}
+
+export const deleteUser = async (req,res,next) =>{
+    if(req.user.id !== req.params.userId){
+        console.log(req.user.id)
+        console.log(req.params.userId)
+        return next(errorHandler(403, 'You are not allowed to delete this user'))
+    }
+
+    try{
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json('User has been deleted');
+
+    }catch(error){
+        next(error);
+    }
 }
