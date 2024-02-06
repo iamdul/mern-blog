46073cdd21e6de529564a1799ad7a3b71471b42b
iamdul <kalharidulanga@gmail.com>
2024-02-06 10:22:39 +0530

    complete create a post page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -15,9 +15,10 @@ import {
     signoutSuccess 
 } from '../redux/user/userSlice';
 import {HiOutlineExclamationCircle} from 'react-icons/hi';
+import {Link} from 'react-router-dom'
 
 export default function DashProfile() {
-    const {currentUser, error} = useSelector(state=>state.user)
+    const {currentUser, error, loading} = useSelector(state=>state.user)
     const [imageFile,setImageFile]=useState(null);
     const [imageFileUrl,setImagefileurl]=useState(null);
     const [imageFileUploadProgress,setImageFileUploadProgress] = useState(null);
@@ -194,9 +195,22 @@ export default function DashProfile() {
                 <TextInput type='email' id='email' placeholder='email'
                 defaultValue={currentUser.email} onChange={handleChange} />
                 <TextInput type='password' id='password' placeholder='password' onChange={handleChange}/>
-                <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-                    Update
+                <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}>
+                    {loading ? 'Loading...' : 'Upadate'}
                 </Button>
+                {
+                    currentUser.isAdmin && (
+                        <Link to={'/create-post'}>
+                            <Button
+                                type='button'
+                                gradientDuoTone='purpleToPink'
+                                className='w-full'
+                            >
+                                Create a post
+                            </Button>
+                        </Link>
+                    )
+                }
             </form>
             <div className='text-red-500 flex justify-between mt-5'>
                 <span onClick={()=>setShowModel(true)} className='cursor-pointer'>Delete Account</span>
