c3a72300b529426f99ba28f154dd4413b33d9feb
iamdul <kalharidulanga@gmail.com>
2024-02-07 14:24:42 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -7,6 +7,7 @@ import {Link} from 'react-router-dom'
 export default function DashPosts() {
   const {currentUser} = useSelector((state)=>state.user);
   const [userPosts,setUserPosts] = useState([]);
+  const [showMore,setShowMore] = useState(true);
   console.log(userPosts)
   useEffect(()=>{
     const fetchPosts= async ()=>{
@@ -14,7 +15,10 @@ export default function DashPosts() {
         const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
         const data = await res.json();
         if(res.ok){
-          setUserPosts(data.posts)
+          setUserPosts(data.posts);
+          if(data.posts.length<9){
+            setShowMore(false);
+          }
         }
       }catch(error){
         console.log(error.message);
@@ -24,6 +28,23 @@ export default function DashPosts() {
       fetchPosts();
     }
   },[currentUser._id])
+
+  const handleShowMore = async () =>{
+    const startIndex= userPosts.length;
+    try{
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev)=>[...prev,...data.posts]);
+        if(data.posts.length<9){
+          setShowMore(false)
+        }
+      }
+
+    }catch(error){
+      console.log(error.message)
+    }
+  }
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto scrollbar
     scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 '>
@@ -41,7 +62,7 @@ export default function DashPosts() {
             </Table.HeadCell>
           </Table.Head>
           {userPosts.map((post)=>(
-            <Table.Body className='divide-y'>
+            <Table.Body key={post._id} className='divide-y'>
               <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                 <Table.Cell>
                   {new Date(post.updatedAt).toLocaleDateString()}
@@ -79,7 +100,15 @@ export default function DashPosts() {
 
           ))}
         </Table>
+        {
+          showMore && (
+            <button onClick={handleShowMore} className='w-full text-teal-500 selt-center text-sm py-7'>
+              Show more
+            </button>
+          )
+        }
         </>
+        
       ):(
         <p>You have no posts to show</p>
       )}
