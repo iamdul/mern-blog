8549955623844cf640da08b89ae8300073fe13ea
iamdul <kalharidulanga@gmail.com>
2024-02-08 13:21:02 +0530

    add delete user functionality to the admin user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -51,9 +51,7 @@ export const updateUser= async (req,res,next) =>{
 }
 
 export const deleteUser = async (req,res,next) =>{
-    if(req.user.id !== req.params.userId){
-        console.log(req.user.id)
-        console.log(req.params.userId)
+    if(!req.user.isAdmin && req.user.id !== req.params.userId){
         return next(errorHandler(403, 'You are not allowed to delete this user'))
     }
 
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,7 +8,8 @@ router.get('/test',test);
 router.put('/update/:userId',verifyToken,updateUser);
 router.delete('/delete/:userId',verifyToken,deleteUser);
 router.post('/signout',signout);
-router.get('/getusers',verifyToken,getUsers)
+router.get('/getusers',verifyToken,getUsers);
+
 
 
 export default router;
