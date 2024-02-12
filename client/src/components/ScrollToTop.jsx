d67fda0bbbd2f27d7fbfcaf9a10565edef91f652
iamdul <kalharidulanga@gmail.com>
2024-02-12 18:44:06 +0530

    add comment section to the post page

---
+++ client/src/components/ScrollToTop.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = () =>{
+    const {pathname} = useLocation();
+    useEffect(()=>{
+        window.scrollTo(0,0);
+    },[pathname])
+    return null;
+};
+
+export default ScrollToTop;
