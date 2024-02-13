d1cf5ab84f18629ee14319fe3e1443a73c5455e6
iamdul <kalharidulanga@gmail.com>
2024-02-13 10:17:11 +0530

    add like functionality to the comment component

--- api/models/comment.model.js
+++ api/models/comment.model.js
@@ -14,7 +14,7 @@ const commentSchema = new mongoose.Schema({
         required:true,
     },
     likes:{
-        type:[String],
+        type:Array,
         default:[],
     },
     numberOfLikes:{
