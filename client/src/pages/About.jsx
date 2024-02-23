d47cee340d1d86ad48c1d5f0ede2fc783c270a3f
iamdul <kalharidulanga@gmail.com>
2024-02-23 23:10:32 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>About Dazzle's Blog </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem atque veritatis dignissimos? Fugiat sapiente, hic, itaque ut minus dignissimos saepe facere incidunt non suscipit dolores porro obcaecati similique, nihil nesciunt explicabo iste eos. Quos sequi quaerat quibusdam delectus facere maiores placeat neque, officiis nostrum odit in illo possimus numquam recusandae consequuntur enim vel provident adipisci, officia explicabo mollitia rerum minima distinctio reprehenderit! Dolore fugiat ratione reprehenderit culpa possimus hic voluptatibus, rem nesciunt beatae sequi praesentium? Id accusamus sequi laboriosam impedit aspernatur neque veniam excepturi ratione assumenda, odio enim ab fuga quas non quibusdam, incidunt minima. Suscipit iusto laudantium consequatur quae.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea laboriosam eaque sequi vero asperiores debitis reprehenderit odit aliquam! Adipisci, eaque corrupti quis, magnam natus nobis repudiandae in dolorum voluptatum voluptas quos pariatur recusandae at ratione neque amet laborum! Cupiditate ad aspernatur officia iusto cumque. Eveniet voluptatibus exercitationem similique velit a.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolor magnam fuga voluptatibus inventore optio impedit neque tenetur beatae cupiditate id incidunt explicabo perspiciatis autem, vitae repellendus eum ab totam, sint in cum nostrum vero debitis laudantium! Necessitatibus perferendis delectus eaque esse officiis? Doloremque reprehenderit ab esse, aliquam quod nisi!</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center flex-col gap-6 p-3 items-center'>
+      <h1 className='text-3xl font-semibold '>Projects</h1>
+      <p className='text-md text-gray-500'>Description about the content</p>
+      <CallToAction/>
+    </div>
   )
 }
